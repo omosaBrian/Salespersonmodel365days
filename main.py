0d20341b5d111dd6897c69978c8e349ab20435a8
Brian Omosa Omwenga <44875298+maldev3555@users.noreply.github.com>
2023-03-24 12:04:59 +0000

    In this code, we first load the CSV data, preprocess it, train the model, generate the forecast, and save the trained model as a joblib file. Then, we load the saved model, initialize the Flask application, and define a /predict API endpoint that takes the input data as a JSON object, converts it to a DataFrame, generates the prediction using the loaded model, and returns the prediction as a JSON object.

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+from flask import Flask, request, jsonify
 import pandas as pd
-import numpy as np
+import joblib
 from pmdarima.arima import auto_arima
 from sklearn.metrics import mean_absolute_error, mean_squared_error
-import joblib
-
 # read in CSV file
 data = pd.read_csv('prdctslsvatlstngsumry.csv')
 
@@ -30,10 +29,8 @@ forecast = model.predict(n_periods=12)
 # create a dataframe to store the forecasts
 forecast_df = pd.DataFrame({'date': future_months, 'total sales Forecast': forecast})
 forecast_df.set_index('date', inplace=True)
-
 # print out the forecasted net sales for the next 12 months
 print("Forecasted net sales for the Next 12 Months:\n", forecast_df)
-
 # evaluate the performance of the model using advanced metrics
 preds = model.predict(n_periods=len(monthly_sales))
 mae = mean_absolute_error(monthly_sales['net sales'], preds)
@@ -43,6 +40,31 @@ mse = mean_squared_error(monthly_sales['net sales'], preds)
 print("\nPerformance Metrics:")
 print("MAE: ", mae)
 print("MSE: ", mse)
-
 # save the trained ARIMA model as a joblib file
 joblib.dump(model, 'model.joblib')
+
+# Load the saved model
+model = joblib.load('model.joblib')
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+# Define an API endpoint
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get the request data
+    data = request.get_json()
+
+    # Convert the request data into a DataFrame
+    input_data = pd.DataFrame(data, index=[0])
+
+    # Make a prediction
+    forecast = model.predict(input_data)
+
+    # Create a response dictionary
+    response = {
+        'prediction': forecast.tolist()
+    }
+
+    # Return the response as JSON
+    return jsonify(response)
